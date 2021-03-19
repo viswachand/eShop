013fd40667593d0029ce78759f90d1289e1fc6ca
viswachand <viswachand19@gmail.com>
2021-03-19 17:50:53 +0530

    Readme edited

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -11,7 +11,7 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
 
   dispatch({
     type: CART_ADD_ITEM,
-    payload: {
+    payload: { 
       product: data._id,
       name: data.name,
       image: data.image,
